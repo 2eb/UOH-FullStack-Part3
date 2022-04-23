b9afd6fc3d2cc36e98ef5a147a5bd4a97d35683b
Z. B. Cirit <zbcirit@gmail.com>
2022-04-23 13:19:37 +0300

    uibuild

---
+++ README.md
@@ -0,0 +1,1 @@
+https://zeb-phonebook.herokuapp.com/
---
+++ build/precache-manifest.d0c12001ee5de985bcfb3f6a8970bac0.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "683c6da4d4ef347fb29d",
+    "url": "/static/js/main.dc7ad359.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "e859f74b056a2e64ca79",
+    "url": "/static/js/2.9561a7ca.chunk.js"
+  },
+  {
+    "revision": "cb2d45dfb42298d51a17fe567e637863",
+    "url": "/index.html"
+  }
+];
